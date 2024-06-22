6966468ec4eb667d5d9c83d5c645c11b32447046
Sayed Zaheer Abass <144617512+zaheernaqvi72@users.noreply.github.com>
2024-06-22 13:46:20 +0530

    Update newsApi.js

--- src/services/newsApi.js
+++ src/services/newsApi.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const apiKey = 'afc2915a01db4e5bbb9fd5a183946655'; 
+const apiKey = 'e3421233278c4b028ddb2656e4fd9fb3'; 
 
 const newsApi = axios.create({
   baseURL: 'https://newsapi.org/v2/',
